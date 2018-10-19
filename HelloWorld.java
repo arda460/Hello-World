a141057be3744f5741e76f7c9614ae2db797c78d
Arda Mutlu <arda460@yahoo.com>
2018-10-19 03:14:42 +0300

    Update HelloWorld.java
    Added two string types to variate the old main method.

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,8 @@ class HelloWorld
 {
   public static void main(String[] args)
   {
-    System.out.println("Hello World!!");
+    string helloString = "Hello ";
+    string worldString = "World";
+    System.out.println(helloString + worldString);
   }
 }
